8bfd8fbeff0bd7ff4aadd4ec9a00f99abf10d3dd
fmrico <fmrico@users.noreply.github.com>
2025-04-15 07:13:47 +0000

    deploy: 12ff062ba176ce37a6239ad2af41751b1a62f2eb

--- search/all_1.js
+++ search/all_1.js
@@ -3,7 +3,7 @@ var searchData=
   ['callbackreturnt_0',['CallbackReturnT',['../classeasynav__ros_1_1EasyNavNode.html#ad09d90f1fe35c9e2f2fd7fc1902312f4',1,'easynav_ros::EasyNavNode::CallbackReturnT'],['../namespaceeasynav__ros.html#ad09d90f1fe35c9e2f2fd7fc1902312f4',1,'easynav_ros::CallbackReturnT']]],
   ['configuration_2ecpp_1',['Configuration.cpp',['../Configuration_8cpp.html',1,'']]],
   ['configuration_2ehpp_2',['Configuration.hpp',['../Configuration_8hpp.html',1,'']]],
-  ['configurationmap_3',['ConfigurationMap',['../namespaceeasynav__core.html#a5d13b9e7017ac4bbdeca3ea776cf4789',1,'easynav_core']]],
+  ['configurationmap_3',['ConfigurationMap',['../namespaceeasynav__core.html#a5d13b9e7017ac4bbdeca3ea776cf4789',1,'easynav_core::ConfigurationMap'],['../namespaceeasynav__ros.html#aa5377dce1258bc94fdd1d676ca85f01c',1,'easynav_ros::ConfigurationMap']]],
   ['configurationvalue_4',['ConfigurationValue',['../classeasynav__core_1_1ConfigurationValue.html',1,'ConfigurationValue'],['../classeasynav__core_1_1ConfigurationValue.html#ab231d85ecc44b3361d23dce166cd4126',1,'easynav_core::ConfigurationValue::ConfigurationValue()'],['../classeasynav__core_1_1ConfigurationValue.html#a64a8e150f2feb20304d5e05b0acd17c2',1,'easynav_core::ConfigurationValue::ConfigurationValue(T value)']]],
   ['configure_5',['configure',['../classeasynav__core_1_1EasyNav.html#ad2c752a704905d116b9a21972214668e',1,'easynav_core::EasyNav']]],
   ['control_5fcycle_6',['control_cycle',['../classeasynav__core_1_1EasyNav.html#a42f68c67ed45423e4f6abc26b3683e38',1,'easynav_core::EasyNav']]],
--- search/all_4.js
+++ search/all_4.js
@@ -5,5 +5,6 @@ var searchData=
   ['get_5flocalizer_2',['get_localizer',['../classeasynav__core_1_1EasyNav.html#acfe61e8e5eabf17108728e80d53dd9a5',1,'easynav_core::EasyNav']]],
   ['get_5fmapper_3',['get_mapper',['../classeasynav__core_1_1EasyNav.html#a92a27df390871f8b0fb042d3fba0e85d',1,'easynav_core::EasyNav']]],
   ['get_5fplanner_4',['get_planner',['../classeasynav__core_1_1EasyNav.html#af7d9de554682a5e8e175b31009cda127',1,'easynav_core::EasyNav']]],
-  ['get_5freal_5ftime_5fcbg_5',['get_real_time_cbg',['../classeasynav__ros_1_1EasyNavNode.html#a8366fef9515c0b0cd47949fbfc018641',1,'easynav_ros::EasyNavNode']]]
+  ['get_5freal_5ftime_5fcbg_5',['get_real_time_cbg',['../classeasynav__ros_1_1EasyNavNode.html#a8366fef9515c0b0cd47949fbfc018641',1,'easynav_ros::EasyNavNode']]],
+  ['get_5fvariant_6',['get_variant',['../classeasynav__core_1_1ConfigurationValue.html#a5c05022f1a36069a31ee02de7d5d9282',1,'easynav_core::ConfigurationValue']]]
 ];
