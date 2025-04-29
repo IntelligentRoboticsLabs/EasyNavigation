03cc5fa1b6d8f786e6d24b7ca5d42de91d8976dd
Juancams <Juancams@users.noreply.github.com>
2025-04-29 12:10:41 +0000

    deploy: d9adab59777be85f33d3a5ac71a3942f678569ce

--- search/classes_5.js
+++ search/classes_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['perception_0',['Perception',['../structeasynav_1_1Perception.html',1,'easynav']]],
-  ['plannernode_1',['PlannerNode',['../classeasynav_1_1PlannerNode.html',1,'easynav']]]
+  ['perceptiondata_1',['PerceptionData',['../structeasynav_1_1PerceptionData.html',1,'easynav']]],
+  ['plannernode_2',['PlannerNode',['../classeasynav_1_1PlannerNode.html',1,'easynav']]]
 ];
