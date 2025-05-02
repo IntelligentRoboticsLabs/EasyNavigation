397d7e6a5ff7268657a1c59b76a7561526e8f833
fmrico <fmrico@users.noreply.github.com>
2025-05-02 05:27:59 +0000

    deploy: 4f744246ad4c0a7e1f37ca78148e435109d20017

--- search/all_e.js
+++ search/all_e.js
@@ -7,7 +7,7 @@ var searchData=
   ['sensorsnodetestcase_4',['SensorsNodeTestCase',['../classSensorsNodeTestCase.html',1,'']]],
   ['sensorsutils_2ecpp_5',['SensorsUtils.cpp',['../SensorsUtils_8cpp.html',1,'']]],
   ['sensorsutils_2ehpp_6',['SensorsUtils.hpp',['../SensorsUtils_8hpp.html',1,'']]],
-  ['set_5fnav_5fstate_7',['set_nav_state',['../classeasynav_1_1ControllerNode.html#ab236eebc784f0964df7ca06e685f4994',1,'easynav::ControllerNode::set_nav_state()'],['../classeasynav_1_1LocalizerNode.html#ab236eebc784f0964df7ca06e685f4994',1,'easynav::LocalizerNode::set_nav_state()'],['../classeasynav_1_1PlannerNode.html#ab236eebc784f0964df7ca06e685f4994',1,'easynav::PlannerNode::set_nav_state()']]],
+  ['set_5fnav_5fstate_7',['set_nav_state',['../classeasynav_1_1ControllerNode.html#ab236eebc784f0964df7ca06e685f4994',1,'easynav::ControllerNode::set_nav_state()'],['../classeasynav_1_1LocalizerNode.html#ab236eebc784f0964df7ca06e685f4994',1,'easynav::LocalizerNode::set_nav_state()'],['../classeasynav_1_1MapsManagerNode.html#ab236eebc784f0964df7ca06e685f4994',1,'easynav::MapsManagerNode::set_nav_state()'],['../classeasynav_1_1PlannerNode.html#ab236eebc784f0964df7ca06e685f4994',1,'easynav::PlannerNode::set_nav_state()']]],
   ['set_5fstate_8',['set_state',['../classeasynav_1_1SensorsNode.html#a2aa00d17220f5888dfce6c3486e72809',1,'easynav::SensorsNode']]],
   ['setup_9',['SetUp',['../classSensorsNodeTestCase.html#a1b06560e0e01a806b92c2386220d0b57',1,'SensorsNodeTestCase']]],
   ['stamp_10',['stamp',['../structeasynav_1_1Perception.html#a3ace6024cedb403facddccc6842dce24',1,'easynav::Perception']]],
